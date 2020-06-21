97a2a0b96cc86c4741ee2cac3a9bc828bbb84ab6
luzheng <luzheng15@163.com>
2020-06-21 18:31:23 +0800

    error？更新

--- src/main/java/com/demo/springboot/helloworld/controller/SignUpController.java
+++ src/main/java/com/demo/springboot/helloworld/controller/SignUpController.java
@@ -84,6 +84,7 @@ public class SignUpController {
                 Cookie cookie=new Cookie("username",login.getUserAdmin());
                 cookie.setMaxAge(60*5);
                 //将cookie发给浏览器（如果没有这句，cookie就不会发送给客户端）
+                cookie.setSecure(true);
                 response.addCookie(cookie);
                 return "/goods";
 //                return "login/loginPage";
