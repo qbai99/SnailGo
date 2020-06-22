8871e2c8218d29357c1024990a45742889c89e81
Q.BAI <q.bai@foxmail.com>
2020-06-22 16:20:43 +0800

    添加了comment表

---
+++ src/main/java/com/demo/springboot/helloworld/common/domain/Comment.java
@@ -0,0 +1,33 @@
+package com.demo.springboot.helloworld.common.domain;
+
+public class Comment {
+    private Integer commentId;
+
+    private Long goodsId;
+
+    private Integer userId;
+
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+
+    public Long getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
